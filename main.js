45afc25a52d63c540a9bdba3ebae4c2213bfc556
Jeff Winkler <jeffwinkler@Jeffs-MBP.attlocal.net>
2019-01-17 19:19:56 -0500

    Ready to set up first function

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
 // Write your JavaScript code here!
+// Write your JavaScript code here!
+var planets = [
+    ['Pluto', 0.06],
+    ['Neptune', 1.148],
+    ['Uranus', 0.917],
+    ['Saturn', 1.139],
+    ['Jupiter', 2.640],
+    ['Mars', 0.3895],
+    ['Moon', 0.1655],
+    ['Earth', 1],
+    ['Venus', 0.9032],
+    ['Mercury', 0.377],
+    ['Sun', 27.9]
+  ];
+  
+ 
+  // `.forEach` `document.createElement` `document.getElementById` `.appendChild`
+  
+  planets.forEach(calculateWeight)
+  
+  function calculateWeight(weight, planetName) {
+     
+  
+  }
+  
+  function handleClickEvent(e) {
+    // 3. Create a variable called userWeight and assign the value of the user's weight.
+    // var userWeight = whatever input gets put into box
+  
+    // 4. Create a variable called planetName and assign the name of the selected planet from the drop down.
+    // var planetName = assign the name of the selected planet from the drop down
+  
+    // 5. Create a variable called result and assign the value of the new calculated weight.
+    // var result = new calculated weight
+  
+    // 6. Write code to display the message shown in the screenshot.
+    // If you were on X, you would weigh Y 
+  
+  }
+  
+  // 7. Set the #calculate-button element's onclick method to use the handleClickEvent function.
+//   onclick method 
+  
+  // Bonus Challenges
+  // 8. Reverse the drop down order so that the sun is first.
+  // 9. Make it look nice using bootstrap (http://getbootstrap.com/getting-started/)
